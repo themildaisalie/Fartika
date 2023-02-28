799496052056ee113b2e5fdb915a9e31a59fb0a9
decimalcubed <77302966+decimalcubed@users.noreply.github.com>
2023-02-28 17:38:37 +0000

    (refactor) general qol improvements

--- src/StarterPlayerScripts/Setup.client.lua
+++ src/StarterPlayerScripts/Setup.client.lua
@@ -11,24 +11,25 @@ local Modules = ReplicatedFirst:WaitForChild("Modules")
 local VolumetricLib = require(Modules:WaitForChild("VolumetricLib"))
 
 local width = 16
-local height = math.round(width * (camera.ViewportSize.X / camera.ViewportSize.Y)) --Scales with aspect ratio roughly correctly
+local height = math.round(width / (camera.ViewportSize.X / camera.ViewportSize.Y)) --Scales with aspect ratio roughly correctlyaws
 
-local totalAmount = 8
-local totalDepth = 80
+local layerCount = 12
+local totalDepth = 120
+local volumeDensity = 0.2
 
 local layers = {}
-for x = 1, totalAmount do
+for x = 1, layerCount do
 	table.insert(layers, VolumetricLib.BuildFrustrumLayer({
 		width = width;
 		height = height;
-		depth_studs = (totalDepth / totalAmount) * x;
+		depth_studs = (totalDepth / layerCount) * x;
 
-		density = 0.22;
+		density = math.sqrt(volumeDensity / layerCount); --TODO find equation to get the required density/transparency per layer for all layers to equal volumeDensity
 		light_influence = 1;
 	}))
 end
 
-game:GetService("RunService"):BindToRenderStep("UpdateVolumetricFog", 100, function()
+game:GetService("RunService").RenderStepped:Connect(function()
 	for x, i in layers do
 		i:Update()
 	end
