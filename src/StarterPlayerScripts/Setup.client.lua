a83c216b8297dc9b8ed5447cb17bbb60990e760d
milda <144637581+themildaisalie@users.noreply.github.com>
2024-01-24 22:46:07 -0600

    Update Setup.client.lua to work with my garbage

--- src/StarterPlayerScripts/Setup.client.lua
+++ src/StarterPlayerScripts/Setup.client.lua
@@ -4,33 +4,47 @@
 --- EXAMPLE INIT SCRIPT
 --- ////////////////////``
 
-local camera = workspace.CurrentCamera
+--ORGINAL MADE BY @DecimalCubed
+--MODIFIED BY @TheMildaIsALie
+
+--enjoy poorly made volumetrics :D
+
+--local camera = workspace.CurrentCamera
 
 local ReplicatedFirst = game:GetService("ReplicatedFirst")
 local Modules = ReplicatedFirst:WaitForChild("Modules")
 local VolumetricLib = require(Modules:WaitForChild("VolumetricLib"))
 
-local width = 16
-local height = math.round(width / (camera.ViewportSize.X / camera.ViewportSize.Y)) --Scales with aspect ratio roughly correctlyaws
+ReplicatedFirst:SetAttribute("UpdateFrustrum", true)
+ReplicatedFirst:SetAttribute("SizeModifier", 6)
+
+local width = 9
+local height = 9
 
 local layerCount = 12
-local totalDepth = 120
-local volumeDensity = 0.2
+local totalDepth = 192
+local volumeDensity = 0.1
 
 local layers = {}
 for x = 1, layerCount do
-	table.insert(layers, VolumetricLib.BuildFrustrumLayer({
-		width = width;
-		height = height;
-		depth_studs = (totalDepth / layerCount) * x;
+	task.defer(function()
+		table.insert(layers, VolumetricLib.NewBuildFrustrumLayer({
+			width = width;
+			height = height;
+			depth_studs = (totalDepth / layerCount) * x;
 
-		density = math.sqrt(volumeDensity / layerCount); --TODO find equation to get the required density/transparency per layer for all layers to equal volumeDensity
-		light_influence = 1;
-	}))
+			density = math.pow(math.pow(volumeDensity,3) / layerCount, 1/3); --TODO find equatzion to get the required density/transparency per layer for all layers to equal volumeDensity
+			light_influence = 1;
+		}))
+	end)
 end
 
+
+
 game:GetService("RunService").RenderStepped:Connect(function()
-	for x, i in layers do
-		i:Update()
+	if ReplicatedFirst:GetAttribute("UpdateFrustrum") == false then return end
+	local SizeModifier = ReplicatedFirst:GetAttribute("SizeModifier")
+	for _, i in layers do
+		i:Update(SizeModifier)
 	end
 end)
